c2aa99d2af6fdc98a15c3e10341d0952599db6ef
Sofiane <slarbi10@stuy.edu>
2020-05-22 08:02:31 -0400

    s

--- tracery/celery.py
+++ tracery/celery.py
@@ -2,13 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracery')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracery.settings')
 app = Celery('tracery')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace="CELERY")
 app.autodiscover_tasks()
-app.conf.beat_schedule = {
-    'display_time-30-seconds': {
-        'task': 'demoapp.tasks.display_time',
-        'schedule': 10.0
-    },
-}
