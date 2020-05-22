f1caaac7c5b4893f38e835e3e07e849a8b72d4cb
Sofiane <slarbi10@stuy.edu>
2020-05-22 06:15:02 -0400

    views

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
-    path('/password/reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset'),
-    path('/password/reset/', PasswordResetDoneView.as_view(template_name='regisuserstration/password_reset_done.html'), name='password_reset_done'),
-    path('/password/reset/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('/password/reset/', PasswordResetCompleteView.as_view(template_name='users/password_reset_comlete.html'), name='password_reset_complete'),
+    path('password/reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset'),
+    path('password/reset/', PasswordResetDoneView.as_view(template_name='regisuserstration/password_reset_done.html'), name='password_reset_done'),
+    path('password/reset/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password/reset/', PasswordResetCompleteView.as_view(template_name='users/password_reset_comlete.html'), name='password_reset_complete'),
 ]
