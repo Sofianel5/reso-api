25b38ea03316bc6e4608cdbeb93f8791d5a450b0
Sofiane <slarbi10@stuy.edu>
2020-05-22 09:36:33 -0400

    bug fix

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
-    path('password/reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html'), html_email_template_name='users/password_reset_email.html', name='password_reset'),
+    path('password/reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html', html_email_template_name='users/password_reset_email.html'), name='password_reset'),
     path('password/reset/', PasswordResetDoneView.as_view(template_name='regisuserstration/password_reset_done.html'), name='password_reset_done'),
     path('password/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password/reset/', PasswordResetCompleteView.as_view(template_name='users/password_reset_comlete.html'), name='password_reset_complete'),
