8c638cdc2133ac9af7cfd0c2e353d8483707a6aa
Sofiane <slarbi10@stuy.edu>
2020-05-22 09:53:59 -0400

    urls saved

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, 
 
 urlpatterns = [
     path('password/reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html', html_email_template_name='users/password_reset_email.html', subject_template_name="users/password_reset_subject.txt"), name='password_reset'),
-    path('password/reset/', PasswordResetDoneView.as_view(template_name='regisuserstration/password_reset_done.html'), name='password_reset_done'),
+    path('password/reset/done', PasswordResetDoneView.as_view(template_name='regisuserstration/password_reset_done.html'), name='password_reset_done'),
     path('password/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password/reset/', PasswordResetCompleteView.as_view(template_name='users/password_reset_comlete.html'), name='password_reset_complete'),
+    path('password/reset/complete', PasswordResetCompleteView.as_view(template_name='users/password_reset_comlete.html'), name='password_reset_complete'),
 ]
