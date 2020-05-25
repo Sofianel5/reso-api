376174e28f1aa18bf5e64d8cf4b974b2aed10276
Sofiane <slarbi10@stuy.edu>
2020-05-25 16:08:13 -0400

    stuff

--- api/urls.py
+++ api/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path("me/scan/", views.UserScanManager.as_view()),
     path("venues/<int:pk>/scan/", views.VenueScanManager.as_view()),
     path("me/registrations/", views.UserBookingsManager.as_view()),
-    path("fixtures/", views.Fixture.as_view()),
     path("myadmin/", views.VenueAdminLogin.as_view()),
     path("myadmin/venues/<int:pk>/timeslots/", views.VenueAdminTimeSlotInfo.as_view()),
 ]
