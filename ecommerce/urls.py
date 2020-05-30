570fdc8c375fc5289d9cf2f013079daeb79d047e
Sofiane <slarbi10@stuy.edu>
2020-05-30 14:21:30 -0400

    removed from urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("signin/", auth_views.LoginView.as_view(template_name='ecommerce/signin.html', authentication_form=forms.SignInForm), name="signin"),
     path("signout/", auth_views.LogoutView.as_view(), name="users/signout"),
     path("", views.home, name="home"),
-    path("pricing/", views.pricing, name="pricing"),
     path("subscriptions/", views.choose_subscription, name="subscriptions"),
     path("checkout/", views.subscribe, name="subscribe"),
     path("enterprise/", views.enterprise, name="enterprise")
