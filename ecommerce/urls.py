8b40c522c19f096f1c6c5ee98d2ede5fd0c3941d
Sofiane <slarbi10@stuy.edu>
2020-05-29 23:47:58 -0400

    path ''

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("signin/", auth_views.LoginView.as_view(template_name='ecommerce/signin.html'), name="signin"),
     path("signout/", auth_views.LogoutView.as_view(), name="users/signout"),
-    path("/", views.home, name="home")
+    path("", views.home, name="home")
 ]
