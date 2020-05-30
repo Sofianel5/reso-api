470f5b6933d165f820a23d707c2ef5f65291e216
Sofiane <slarbi10@stuy.edu>
2020-05-30 00:26:00 -0400

    idek

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -7,8 +7,8 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'autocomplete':'off'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': "First name", 'required':'required'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': "Last name", 'required':'required'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': "Last name", 'required':'required'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': "Last name", 'required':'required'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': "Password", 'required':'required'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': "Password (confirm)", 'required':'required'}))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
