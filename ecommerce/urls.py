843c495e94767dc6720a38e47d922104540a64b6
Sofiane <slarbi10@stuy.edu>
2020-05-29 23:27:40 -0400

    import

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from ecommerce import views as ecommerce_views
+from .views import *
 
 urlpatterns = [
     path("signup/", auth_views.signup, name="signup"),
     path("signin/", auth_views.LoginView.as_view(template_name='ecommerce/signin.html'), name="signin"),
     path("signout/", auth_views.LogoutView.as_view(), name="users/signout"),
-    path("home/", ecommerce_views.home, name="home")
+    path("home/", home, name="home")
 ]
