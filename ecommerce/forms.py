be3651da2348fe27daf2b99b346d790909c3bf49
Sofiane <slarbi10@stuy.edu>
2020-05-30 01:21:24 -0400

    waht

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -14,7 +14,7 @@ class UserRegisterForm(UserCreationForm):
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
 
 class SignInForm(AuthenticationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'autocomplete':'off'}))
+    username = forms.EmailField(widget=forms.EmailInput(attrs={'autocomplete':'off', 'class':'form-control', 'placeholder': "Email"}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder': "Password", 'required':'required'}))
 
 class VenueCreationForm(forms.ModelForm):
