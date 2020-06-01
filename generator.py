2fbaaa5ea9a11f9529ad1355c8c58b1c5cda4bcb
Jude Rizzo <jrizzo00@stuy.edu>
2020-06-01 10:43:57 -0400

    geenerator

---
+++ generator.py
@@ -0,0 +1,21 @@
+from venues.models import TimeSlot
+import datetime
+
+def dayH(startt, endt, interval, venuet, mt, type1="All"):
+    counter = startt
+    temp = 0
+    while (counter < endt):
+        temp = TimeSlot(venue =  venuet, start = counter, end = counter + datetime.timedelta(minutes = interval), max_attendees = mt, attendees = 0, type = type1)
+        temp.save()
+        print(temp)
+        counter += datetime.timedelta(minutes = interval)
+
+
+
+def day(magic, type="All"):
+    dayH(magic[0], magic[1], magic[2], magic[3],magic[4],type)
+
+
+def week(list):
+    for i in list:
+        day(i)
